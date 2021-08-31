0104324d58aaf1b431b1b3b04c3c7369f77d1c56
Saeed <saeed_et90@yahoo.com>
2021-08-31 23:53:49 +1000

    selected ghost added

--- components/SidebarRow.js
+++ components/SidebarRow.js
@@ -2,13 +2,18 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setSelectedGhost } from "../redux/features/ghostSlice";
+import {
+  selectSelectedGhost,
+  setSelectedGhost,
+} from "../redux/features/ghostSlice";
 import { selectUser } from "../redux/features/userSlice";
 
 function SidebarRow({ src, title }) {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
+  const selectedGhost = useSelector(selectSelectedGhost);
 
+  let bg = "";
   const clickHandler = (name, image) => {
     if (user?.displayName == name) {
       dispatch(setSelectedGhost(null));
@@ -21,11 +26,15 @@ function SidebarRow({ src, title }) {
       );
     }
   };
-
+  if (!selectedGhost && user?.displayName == title) {
+    bg = "bg-gray-200";
+  }
   return (
     <div
       onClick={() => clickHandler(title, src)}
-      className="flex items-center space-x-2 p-2 hover:bg-gray-200 rounded-[7px] cursor-pointer"
+      className={`flex items-center space-x-2 p-2 ${bg}  ${
+        selectedGhost?.name == title && "bg-gray-200"
+      } hover:bg-gray-200 rounded-[7px] cursor-pointer`}
     >
       <Avatar src={src} className="h-8 w-8" />
       <p className="hidden sm:inline-flex font-medium">{title}</p>
